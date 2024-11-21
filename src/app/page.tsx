69e6fc61544c95073182faac37618eeffc0f377e
PhamMinhVuong21020425 <phamminhv26@gmail.com>
2024-11-21 16:53:20 +0700

    Fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="text-2xl font-bold text-red-500">Smart Heart</div>;
+    return <div className="text-2xl font-bold text-red-500">Smart Health</div>;
 }
