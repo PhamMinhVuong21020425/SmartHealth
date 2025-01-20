7e2342a653925d96644792c62605e050a03b3447
PhamMinhVuong21020425 <phamminhv26@gmail.com>
2025-01-20 13:19:54 +0700

    Add dialog component and enhance image slider with full-size view functionality

--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 const ImageSlider = ({ images }: { images: string[] }) => {
   // Product images state
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   // Image slider handlers
   const nextImage = () => {
@@ -30,7 +32,8 @@ const ImageSlider = ({ images }: { images: string[] }) => {
             alt={`Product Image ${currentImageIndex + 1}`}
             width={500}
             height={500}
-            className="h-48 md:h-60 lg:h-72 w-auto rounded-md"
+            className="h-48 md:h-60 lg:h-72 w-auto rounded-md cursor-pointer"
+            onClick={() => setDialogOpen(true)}
           />
         </div>
         {images.length > 1 && (
@@ -77,6 +80,40 @@ const ImageSlider = ({ images }: { images: string[] }) => {
           ))}
         </div>
       </div>
+
+      {/* Full-size Image Dialog */}
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent className="max-w-screen-lg w-full p-0 bg-black/90 [&>button]:text-white [&>button]:font-bold [&>button]:p-1 [&>button]:bg-black/90 [&>button]:top-4 [&>button]:right-4">
+          <div className="relative flex items-center justify-center min-h-96">
+            <div className="relative w-full h-full flex items-center justify-center">
+              <Image
+                src={images[currentImageIndex]}
+                alt={`Full size image ${currentImageIndex + 1}`}
+                width={1200}
+                height={800}
+                className="rounded-md object-contain max-h-[80vh]"
+              />
+
+              {images.length > 1 && (
+                <>
+                  <button
+                    onClick={prevImage}
+                    className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-black/50 hover:bg-black/75 p-2 rounded-full"
+                  >
+                    <ChevronLeft className="text-white" />
+                  </button>
+                  <button
+                    onClick={nextImage}
+                    className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black/50 hover:bg-black/75 p-2 rounded-full"
+                  >
+                    <ChevronRight className="text-white" />
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
